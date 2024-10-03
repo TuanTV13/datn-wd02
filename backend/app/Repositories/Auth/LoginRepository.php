2aaae57057527d12f33bb3a8c0b0762733b51c08
TuanTV13 <tvtuan01032004@gmail.com>
2024-10-03 13:48:04 +0700

    login+register

--- backend/app/Repositories/Auth/LoginRepository.php
+++ backend/app/Repositories/Auth/LoginRepository.php
@@ -6,9 +6,11 @@ use App\Models\User;
 
 class LoginRepository
 {
+
     public function findByEmail($email)
     {
         // Trả về user đầu tiên có email giống với email nhập vào
         return User::with(['province', 'district', 'ward'])->where('email', $email)->first();
     }
+    
 }
