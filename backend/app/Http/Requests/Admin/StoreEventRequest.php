d3f915c63c6f20b0e89a777d8da2ebf847aa6e55
TuanTV13 <tvtuan01032004@gmail.com>
2024-10-07 23:20:19 +0700

    main

--- backend/app/Http/Requests/Admin/StoreEventRequest.php
+++ backend/app/Http/Requests/Admin/StoreEventRequest.php
@@ -15,35 +15,35 @@ class StoreEventRequest extends FormRequest
     {
         $startTime = $this->input('event.start_time');
         return [
-            'event' => 'required_array_keys:category_id,province_id,district_id,ward_id,name,description,start_time,end_time,location,event_type,max_attendees',
+            'event' => 'array|required_array_keys:category_id,status_id,province_id,district_id,ward_id,name,description,start_time,end_time,location,event_type,max_attendees',
 
-            'event.category_id'                     => 'required|exists:categories,id',
-            // 'event.status_id'                       => 'required|exists:statuses,id',
-            'event.province_id'                     => 'required|exists:provinces,id',
-            'event.ward_id'                         => 'required|exists:wards,id',
-            'event.district_id'                     => 'required|exists:districts,id',
+            'event.category_id'                     => 'required|integer',
+            'event.status_id'                       => 'required|integer',
+            'event.province_id'                     => 'required|integer',
+            'event.district_id'                     => 'required|integer',
+            'event.ward_id'                         => 'required|integer',
             'event.name'                            => 'required|string|max:255',
             'event.description'                     => 'required|string',
-            'event.start_time'                      => 'required|date|after:' . now()->addDays(7),
-            'event.end_time'                        => 'required|date|after:event.start_time',
+            'event.start_time'                      => 'required|date|after_or_equal:' . now(),
+            'event.end_time'                        => 'required|date|after_or_equal:' . Carbon::parse($startTime)->addHours(1),
             'event.location'                        => 'required|string|max:255',
             'event.event_type'                      => 'required|in:online,offline',
             'event.link_online'                     => 'nullable|url|active_url',
             'event.max_attendees'                   => 'required|integer|min:0',
 
-            // Diễn giả
             'speakers'                              => 'nullable|array',
             'speakers.*.name'                       => 'required|string|max:255',
-            'speakers.*.email'                      => 'required|email|unique:speakers,email',
-            'speakers.*.phone'                      => 'required|string',
+            'speakers.*.email'                      => 'required|string|email|unique:speakers,email',
+            'speakers.*.phone'                      => ['required', 'string', 'regex:/^(0[3|5|7|8|9])[0-9]{8}$/', 'unique:speakers,phone'],
             'speakers.*.profile'                    => 'nullable|string',
-            'speakers.*.image_url'                  => 'nullable|string',
-            'event_images'                          => 'nullable|array',
-            'event_images.*'                        => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            'speakers.*.image_url'                  => 'nullable|image|mimes:jpeg,png,jpg,gif',
 
-            // Vé
+            'event_images'                          => 'required|array',
+            'event_images.*'                        => 'required|image|mimes:jpeg,png,jpg,gif',
+
             'tickets'                               => 'nullable|array',
             'tickets.*.ticket_type_id'              => 'required|integer',
+            'tickets.*.status_id'                   => 'required|integer',
             'tickets.*.price'                       => 'required|numeric',
             'tickets.*.quantity'                    => 'required|integer|min:1',
             'tickets.*.available_quantity'          => 'nullable|integer',
@@ -52,6 +52,10 @@ class StoreEventRequest extends FormRequest
             'tickets.*.sale_end'                    => 'required|date|after_or_equal:tickets.*.sale_start|before_or_equal:event.start_time',
             'tickets.*.description'                 => 'nullable|string',
 
+            'tickets.*.vouchers'                    => 'nullable|array',
+            'tickets.*.vouchers.*.code'             => 'required|string|unique:vouchers,code',
+            'tickets.*.vouchers.*.discount_amount'  => 'required|numeric',
+            'tickets.*.vouchers.*.expiration_date'  => 'required|date',
         ];
     }
 
@@ -72,21 +76,24 @@ class StoreEventRequest extends FormRequest
             'event.ward_id'                         => 'Phường',
             'event.name'                            => 'Tên sự kiện',
             'event.description'                     => 'Mô tả sự kiện',
-            'event.start_time'                      => 'Thời gian bắt đầu',
-            'event.end_time'                        => 'Thời gian kết thúc',
+            'event.start_time'                      => 'Thời gian bắt đầu sự kiện',
+            'event.end_time'                        => 'Thời gian kết thúc sự kiện',
             'event.location'                        => 'Địa điểm',
             'event.event_type'                      => 'Loại sự kiện',
+            'event.link_online'                     => 'Link online',
             'event.max_attendees'                   => 'Số lượng người tham gia tối đa',
 
             'speakers.*.name'                       => 'Tên diễn giả',
             'speakers.*.email'                      => 'Email diễn giả',
-            'speakers.*.phone'                      => 'Điện thoại diễn giả',
+            'speakers.*.phone'                      => 'Số điện thoại diễn giả',
             'speakers.*.profile'                    => 'Hồ sơ diễn giả',
             'speakers.*.image_url'                  => 'Ảnh diễn giả',
 
+            'event_images'                          => 'Hình ảnh sự kiện',
             'event_images.*'                        => 'Hình ảnh sự kiện',
 
             'tickets.*.ticket_type_id'              => 'Loại vé',
+            'tickets.*.status_id'                   => 'Trạng thái vé',
             'tickets.*.price'                       => 'Giá vé',
             'tickets.*.quantity'                    => 'Số lượng vé',
             'tickets.*.available_quantity'          => 'Số lượng vé có sẵn',
