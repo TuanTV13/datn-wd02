ad2dc12cef69b58a280f81c1f6738de2f0108080
TuanTV13 <tvtuan01032004@gmail.com>
2024-10-13 12:04:13 +0700

    check-conflig

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RolePermissionController;
 use App\Http\Controllers\V1\AuthController;
 use App\Http\Controllers\V1\EventController;
 use App\Http\Controllers\V1\TicketController;
+use App\Http\Controllers\V1\UserController; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
