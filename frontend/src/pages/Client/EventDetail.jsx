8cfce27d675a7655906c5617f57d53bdcd989641
TuanTV13 <tvtuan01032004@gmail.com>
2024-11-19 17:57:12 +0700

    abc

--- frontend/src/pages/Client/EventDetail.jsx
+++ frontend/src/pages/Client/EventDetail.jsx
@@ -9,7 +9,7 @@ const EventDetail = () => {
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [similarEvents, setSimilarEvents] = useState([]);
   useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/api/v1/clients/events/${id}`)
+    axios.get(`http://192.168.2.112/api/v1/clients/events/${id}`)
 
       .then((response) => {
         setEvent(response.data.data);
