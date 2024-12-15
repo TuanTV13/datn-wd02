5ea71dc3a5f982033be4b81b971a5fe0a6267bf8
0345470057 <dacanh999@gmail.com>
2024-12-15 18:21:41 +0700

    ÁNH

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -185,7 +185,7 @@ Route::prefix('v1')->group(function () {
             Route::get('/top-revenue', [StatisticsController::class, 'topRevenueEvents']);
             // Route để lấy thống kê số sự kiện hoàn thành trong khoảng thời gian
             Route::get('/event-statistics', [StatisticsController::class, 'getEventStatisticsByTime']);
-
+              // Route để lấy thống kê sự kiện theo thể loại (chỉ sự kiện đã được xác nhận)
             Route::get('/statistics-by-category', [StatisticsController::class, 'getEventCountTotalAmountAndPercentageByEventType']);
 
             
