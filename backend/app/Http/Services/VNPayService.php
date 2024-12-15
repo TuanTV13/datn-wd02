50b3d019d7c18d28a53d534b2dab1003ded77542
lam <doquang227@gmail.com>
2024-12-15 15:25:28 +0700

    fix return vnpay

--- backend/app/Http/Services/VNPayService.php
+++ backend/app/Http/Services/VNPayService.php
@@ -16,7 +16,7 @@ class VNPayService
         $vnp_HashSecret = env('VNPAY_HASH_SECRET');
 
         $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-        $vnp_Returnurl = 'http://localhost:5173/order-detail/' + $transaction_id;
+        $vnp_Returnurl = "http://127.0.0.1:8000/return-vnpay/" . '?transaction_id=' . $transaction_id;
         $vnp_TxnRef = date("YmdHis");
         $vnp_OrderInfo = "Thanh toán hóa đơn phí dịch vụ";
         $vnp_OrderType = 'billpayment';
